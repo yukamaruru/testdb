56a2c4c8cb6dd9de4eeaf3b8a60fe2adf347f188
yukamaru <yuun.vv07@gmail.com>
2025-06-24 22:23:41 +0900

    jdbcドライバ追加

--- src/testdb/Checkloadjdbc.java
+++ src/testdb/Checkloadjdbc.java
@@ -2,9 +2,19 @@ package testdb;
 
 public class Checkloadjdbc {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws
+	InstantiationException,IllegalAccessException {
 		// TODO 自動生成されたメソッド・スタブ
-
+		String msg = "";
+		
+		try{
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			msg = "ドライバのロードに成功しました";
+		}catch(ClassNotFoundException e){
+			msg = "ドライバのロードに失敗しました";
+		}
+		System.out.println(msg);
 	}
+	
 
 }
