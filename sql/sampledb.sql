7a41176b643d0f6056607e8e267b293a1da5aca0
yukamaru <yuun.vv07@gmail.com>
2025-06-30 20:05:37 +0900

    テーブルsample_table作成

---
+++ sql/sampledb.sql
@@ -0,0 +1,12 @@
+drop database if exists sampledb;
+create database sampledb;
+use sampledb;
+
+create table sample_table(
+user_number int,
+user_name varchar(255),
+user_age int,
+user_mail varchar(50)
+);
+
+
