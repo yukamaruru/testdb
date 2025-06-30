a4d397c6dae954a6b5dac1cca3937280b26dd9c2
yukamaru <yuun.vv07@gmail.com>
2025-06-30 20:08:53 +0900

    insert

--- sql/sampledb.sql
+++ sql/sampledb.sql
@@ -9,4 +9,8 @@ user_age int,
 user_mail varchar(50)
 );
 
+insert into sample_table values(1,"Yamada",21,"xxx@yahoo.co.jp");
+insert into sample_table values(2,"Sato",39,"yyy@google.com");
+insert into sample_table values(3,"Harada",44,"zzz@yahoo.co.jp");
+
 
