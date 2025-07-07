3857140a8bf144e2924198747e0ea222a5278e5a
yukamaru <yuun.vv07@gmail.com>
2025-07-07 21:11:08 +0900

    testuserdaoクラス追記

--- src/testdb/TestUserDAO.java
+++ src/testdb/TestUserDAO.java
@@ -35,4 +35,26 @@ public void select(String name,String password){
 	}
 }
 
+public void selectAll(){
+	DBConnector db = new DBConnector();
+	Connection con = db.getConnection();
+
+	String sql = "select * from test_table";
+	try{
+		PreparedStatement ps = con.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		while(rs.next()){
+			System.out.println(rs.getString("user_name"));
+			System.out.println(rs.getString("password"));
+		}
+	}catch(SQLException e){
+		e.printStackTrace();
+	}
+	try{
+		con.close();
+	}catch(SQLException e){
+		e.printStackTrace();
+	}
+}
+
 }
