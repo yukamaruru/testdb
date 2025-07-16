0eeb7430b4c253ce2bfef75e7d6ae86b1fdd6d0d
yukamaru <yuun.vv07@gmail.com>
2025-07-16 20:23:38 +0900

    testuserdaoクラス更新

--- src/testdb/TestUserDAO.java
+++ src/testdb/TestUserDAO.java
@@ -152,7 +152,27 @@ public void insert(int user_id,String name, String password){
 	}
 }
 
+public void delete(String name){
+	DBConnector db = new DBConnector();
+	Connection con = db.getConnection();
 
+	String sql = "delete from test_table where user_name=?";
+	try{
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setString(1, name);
+		int i=ps.executeUpdate();
+		if(i>0){
+			System.out.println(i + "件削除されました");
+		}
+	}catch(SQLException e){
+		e.printStackTrace();
+	}
+	try{
+		con.close();
+	}catch(SQLException e){
+		e.printStackTrace();
+	}
+}
 
 
 
