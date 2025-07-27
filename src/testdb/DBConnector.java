5df7f01694a81b66626825986fac365d2ae859c7
yukamaru <yuun.vv07@gmail.com>
2025-07-27 21:47:04 +0900

    DBConnectorクラス作成

--- src/testdb/DBConnector.java
+++ src/testdb/DBConnector.java
@@ -1,33 +1,5 @@
 package testdb;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class DBConnector {
-	private static String driverName = "com.mysql.jdbc.Driver";
-	//jdbcのドライバーの名前を変数に代入している。ドライバーはJavaとデータベースを繋げる工具箱。
-
-	private static String url = "jdbc:mysql://localhost/testdb?autoReconnect=true&useSSL=false";
-
-	private static String user = "root";
-	private static String password = "root";
-
-public Connection getConnection(){
-//接続状態にする
-	Connection con = null;
-	//一度状態を初期化。
-	//↑この二つは接続しかしないクラス。公式として覚える。
-
-	try{
-		Class.forName(driverName);
-		con = DriverManager.getConnection(url,user,password);
-	}catch(ClassNotFoundException e){
-		e.printStackTrace();
-	}catch(SQLException e){
-		e.printStackTrace();
-	}
-	return con;
-}
 
 }
