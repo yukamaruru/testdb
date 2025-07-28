a8a1328527119903073076469468e224707bd275
yukamaru <yuun.vv07@gmail.com>
2025-07-28 20:26:30 +0900

    TestUserDAOクラス更新

--- src/testdb/TestUserDAO.java
+++ src/testdb/TestUserDAO.java
@@ -56,4 +56,55 @@ public class TestUserDAO {
 		}
 	}
 
+	public void selectByName(String name){
+		DBConnector db = new DBConnector();
+		Connection con = db.getConnection();
+
+		String sql = "select * from test_table where user_name=?";
+		try{
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1,name);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()){
+				System.out.println(rs.getString("user_name"));
+				System.out.println(rs.getString("password"));
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		try{
+			con.close();
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
